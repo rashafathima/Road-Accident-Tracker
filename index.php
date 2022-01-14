6db56998e829cc21eb9baf0ce684804325eb30ab
Neha_Hegde <54177691+Nehahegde@users.noreply.github.com>
2022-01-14 17:45:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -19,3 +19,25 @@ $query = "INSERT INTO ` User`(`ID`, `Username`, `password`) VALUES ('$UID','$use
 mysqli_query($con, $query);
 
 echo $query;
+
+#For location table-- 
+<?php
+$con = mysqli_connect('localhost', 'root');
+
+if($con){
+    echo "Connection successful";
+}else{
+    echo "Connection failed";
+}
+
+mysqli_select_db($con, 'Accident_Hotspot_db');
+
+$Street =$_POST['Street'];
+$pincode =$_POST['Pincode'];
+$zone =$_POST['Zone'];
+
+$query = "INSERT INTO ` Location`(``, `Username`, `password`) VALUES ('$UID','$username','$password')";
+
+mysqli_query($con, $query);
+
+echo $query;
